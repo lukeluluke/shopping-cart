4cb189059ffb398cd727d7c61f01f4f91aee78cf
lukeluluke <luke0427@hotmail.com>
2019-08-30 22:42:34 +1000

    Add test json files

---
+++ command.js
@@ -0,0 +1,10 @@
+"use strict"
+const program = require('commander')
+
+program
+    .version('1.0.0')
+    .description('Shopping Cart Calculation Tool')
+    .parse(process.argv)
+
+
+module.exports = program
