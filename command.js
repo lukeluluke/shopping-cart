d9487567a2fbd8c894d687273f9a3d506a560f0c
lukeluluke <luke0427@hotmail.com>
2019-08-30 22:53:00 +1000

    Add cli command with options

--- command.js
+++ command.js
@@ -4,6 +4,9 @@ const program = require('commander')
 program
     .version('1.0.0')
     .description('Shopping Cart Calculation Tool')
+    .option('-d --debug', 'show debug information')
+    .option('-c --cart [file name]', 'name of cart json file', '')
+    .option('-p --price [file name]', 'name of base price json file', '')
     .parse(process.argv)
 
 
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 "use strict"
 const program = require('./command')
 
+if(program.debug) {
+    console.log('Debug is enabled')
+}
+
+if (!program.cart || !program.price) {
+    console.log('Files missing')
+}
+
+
+
 
 
